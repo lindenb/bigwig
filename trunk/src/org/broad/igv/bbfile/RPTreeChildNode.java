07702936676ae2f4afecfd87a90d23ca34a3c38a
jrobinso@broadinstitute.org <jrobinso@broadinstitute.org@6be1cf3b-3429-8828-5e03-2cae64931a3a>
2011-06-22 21:59:29 +0000

    Update copyright

--- trunk/src/org/broad/igv/bbfile/RPTreeChildNode.java
+++ trunk/src/org/broad/igv/bbfile/RPTreeChildNode.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2007-2011 by The Broad Institute of MIT and Harvard.  All Rights Reserved.
+ *
+ * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
+ * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS." THE BROAD AND MIT MAKE NO REPRESENTATIONS OR
+ * WARRANTES OF ANY KIND CONCERNING THE SOFTWARE, EXPRESS OR IMPLIED, INCLUDING,
+ * WITHOUT LIMITATION, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, NONINFRINGEMENT, OR THE ABSENCE OF LATENT OR OTHER DEFECTS, WHETHER
+ * OR NOT DISCOVERABLE.  IN NO EVENT SHALL THE BROAD OR MIT, OR THEIR RESPECTIVE
+ * TRUSTEES, DIRECTORS, OFFICERS, EMPLOYEES, AND AFFILIATES BE LIABLE FOR ANY DAMAGES
+ * OF ANY KIND, INCLUDING, WITHOUT LIMITATION, INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ * ECONOMIC DAMAGES OR INJURY TO PROPERTY AND LOST PROFITS, REGARDLESS OF WHETHER
+ * THE BROAD OR MIT SHALL BE ADVISED, SHALL HAVE OTHER REASON TO KNOW, OR IN FACT
+ * SHALL KNOW OF THE POSSIBILITY OF THE FOREGOING.
+ */
+
 package org.broad.igv.bbfile;
 
 import org.apache.log4j.Logger;
@@ -20,13 +38,10 @@ public class RPTreeChildNode implements RPTreeNode {
 
     private static Logger log = Logger.getLogger(RPTreeChildNode.class);
 
-    private long nodeIndex;        // index for node in R+ tree organization
     private RPChromosomeRegion chromosomeBounds;  // chromosome bounds for entire node
     private ArrayList<RPTreeChildNodeItem> childItems; // array for child items
 
-    public RPTreeChildNode(long nodeIndex) {
-
-        this.nodeIndex = nodeIndex;
+    public RPTreeChildNode() {
         childItems = new ArrayList<RPTreeChildNodeItem>();
 
         // Note: Chromosome bounds are null until a valid region is specified
@@ -34,9 +49,6 @@ public class RPTreeChildNode implements RPTreeNode {
 
     // *** BPTreeNode interface implementation ***
 
-    public long getNodeIndex() {
-        return nodeIndex;
-    }
 
     public RPChromosomeRegion getChromosomeBounds() {
         return chromosomeBounds;
@@ -102,10 +114,7 @@ public class RPTreeChildNode implements RPTreeNode {
 
     public void printItems() {
 
-        log.debug("Child node " + nodeIndex + " contains "
-                + childItems.size() + " items:");
-
-        for (int item = 0; item < childItems.size(); ++item) {
+         for (int item = 0; item < childItems.size(); ++item) {
             childItems.get(item).print();
         }
     }
